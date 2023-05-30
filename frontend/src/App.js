265b676ec3cbf6678d6eeeff38631699ebab9a5f
Trisha-tech <1234trishasahu@gmail.com>
2023-05-31 01:43:05 +0530

    Initial Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Welcome to Civil20 : Full Stack Assignment !!!    Made by Trisha Sahu
+    </div>
+  );
+}
+
+export default App;
